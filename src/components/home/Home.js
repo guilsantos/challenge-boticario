e9a8db95ec59ccc40be179050f212a6cc84aafac
guilherme.santos <guiga021@hotmail.com>
2018-12-12 03:11:06 -0200

    Nova Home

---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const Home = () => (
+    <Fragment>
+        <div>
+            <div>MINHA LOJA</div>|
+            <div>PERFUMARIA</div>
+            <div>MAQUIAGEM</div>
+            <div>CABELOS</div>
+            <div>INFANTIL</div>
+        </div>
+    </Fragment>
+)
+
+export default Home;
