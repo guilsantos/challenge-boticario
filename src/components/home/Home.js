46d88f12345d850a4fd087c3dfb41a8fdc3ab5ce
guilherme.santos <guiga021@hotmail.com>
2018-12-13 02:35:25 -0200

    Adiciona estilos iniciais

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,15 @@
 import React, { Fragment } from 'react'
+import { TopBar, Categories, ShopTitle } from './Home.style'
 
 const Home = () => (
     <Fragment>
-        <div>
-            <div>MINHA LOJA</div>|
-            <div>PERFUMARIA</div>
-            <div>MAQUIAGEM</div>
-            <div>CABELOS</div>
-            <div>INFANTIL</div>
-        </div>
+        <TopBar>
+            <ShopTitle href="#home">MINHA LOJA</ShopTitle>
+            <Categories href="#perfumaria">PERFUMARIA</Categories>
+            <Categories href="#maquiagem">MAQUIAGEM</Categories>
+            <Categories href="#cabelo">CABELOS</Categories>
+            <Categories href="#infantil">INFANTIL</Categories>
+        </TopBar>
     </Fragment>
 )
 
---
+++ src/components/home/Home.style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const TopBar = styled.div`
+    overflow: hidden;
+    background-color: #FCFCFC;
+    border-bottom: #EBEBEB solid;
+    padding-top: 24px;
+    padding-bottom: 24px;
+    font-family: sans-serif;
+`
+
+export const ShopTitle = styled.a`
+    font-size: 20px;
+    border-right: #EBEBEB solid;
+    padding: 12px 24px;
+    text-decoration: none;
+`
+
+export const Categories = styled.a`
+    font-size: 18px;
+    text-decoration: none;
+    padding: 24px;
+`
