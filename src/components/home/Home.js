e5a6e82200379ff69929fb5251a9c7b8f4053270
guilherme.santos <guiga021@hotmail.com>
2018-12-14 05:36:32 -0200

    Adiciona resposividade

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,8 @@ import {
   withState
 } from 'recompose'
 import axios from 'axios'
-import { Wrapper, TopBar, Categories, ShopTitle, BodyTitle, BorderDiv, TitleWrapper, ProductCard, BodyWrapper, ProductImage, ProductName, ProductValue } from './Home.style'
+import { Wrapper, HeaderWrapper, TopBar, Categories, CartImage, CartWrapper, ShopTitle, BodyTitle, BorderDiv, TitleWrapper, ProductCard, BodyWrapper, ProductImage, ProductName, ProductValue, ContentWrapper } from './Home.style'
+import cart from '../../assets/cart.png'
 import { HOME } from './Home.messages'
 
 const URL = "products.json"
@@ -25,27 +26,32 @@ const enhancer = compose(
 
 const Home = ({ products }) => console.log('products', products) || (
 <Wrapper>
-  <TopBar>
-    <ShopTitle href="#home">{HOME.topbar.store}</ShopTitle>
-    <Categories href="#perfumaria">{HOME.topbar.perfumery}</Categories>
-    <Categories href="#maquiagem">{HOME.topbar.makeUp}</Categories>
-    <Categories href="#cabelo">{HOME.topbar.hair}</Categories>
-    <Categories href="#infantil">{HOME.topbar.child}</Categories>
-  </TopBar>
-  <TitleWrapper>
-    <BorderDiv />
-    <BodyTitle>{HOME.body.title}</BodyTitle>
-    <BorderDiv />
-  </TitleWrapper>
-  <BodyWrapper>
-    {products.map(product => (
-      <ProductCard>
-        <ProductImage src={product.images[0].imageUrl} />
-        <ProductName>{product.name}</ProductName>
-        <ProductValue>R$ {product.Value}</ProductValue>
-      </ProductCard>
-    ))}
-  </BodyWrapper>
+  <HeaderWrapper>
+    <TopBar>
+      <ShopTitle href="#home">{HOME.topbar.store}</ShopTitle>
+      <Categories href="#perfumaria">{HOME.topbar.perfumery}</Categories>
+      <Categories href="#maquiagem">{HOME.topbar.makeUp}</Categories>
+      <Categories href="#cabelo">{HOME.topbar.hair}</Categories>
+      <Categories href="#infantil">{HOME.topbar.child}</Categories>
+      <CartWrapper><CartImage src={cart} width={28} height={25} /></CartWrapper>
+    </TopBar>
+  </HeaderWrapper>
+  <ContentWrapper>
+    <TitleWrapper>
+      <BorderDiv />
+      <BodyTitle>{HOME.body.title}</BodyTitle>
+      <BorderDiv />
+    </TitleWrapper>
+    <BodyWrapper>
+      {products.map(product => (
+        <ProductCard>
+          <ProductImage src={product.images[0].imageUrl} />
+          <ProductName>{product.name}</ProductName>
+          <ProductValue>R$ {product.Value}</ProductValue>
+        </ProductCard>
+      ))}
+    </BodyWrapper>
+  </ContentWrapper>
 </Wrapper>
 )
 
--- src/components/home/Home.style.js
+++ src/components/home/Home.style.js
@@ -4,21 +4,36 @@ export const Wrapper = styled.div`
   font-family: sans-serif;
 `
 
-export const TopBar = styled.div`
-  overflow: hidden;
+export const HeaderWrapper = styled.div`
   background-color: #FCFCFC;
   border-bottom: #ECECEC solid 1px;
-  padding-top: 24px;
-  padding-bottom: 24px;
+  padding-top: 42px;
+  padding-bottom: 42px;
+`
+
+export const TopBar = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+
+  width: 1032px;
+  @media (max-width: 1032px) {
+    max-width: 690px;
+  }
+  @media (max-width: 690px) {
+    max-width: 340px;
+  }
 `
 
 export const ShopTitle = styled.a`
   font-size: 20px;
-  border-right: #ECECEC solid 1px;
-  padding: 12px 24px;
+  padding: 12px 38px;
   text-decoration: none;
   color: #666363;
   font-weight: bold;
+
+  @media (min-width: 1032px) {
+    border-right: #ECECEC solid 1px;
+  }
 `
 
 export const Categories = styled.a`
@@ -26,6 +41,34 @@ export const Categories = styled.a`
   text-decoration: none;
   padding: 24px;
   color: #808080;
+  @media (max-width: 1032px) {
+    display: none;
+  }
+`
+
+export const CartImage = styled.img`
+  margin: auto;
+`
+
+export const CartWrapper = styled.div`
+  border: solid 2px #807F7F;
+  display: inline-flex;
+  height: 44px;
+  width: 68px;
+  border-radius: 26px;
+  float: right;
+`
+
+export const ContentWrapper = styled.div`
+  width: 1032px;
+  margin-left: auto;
+  margin-right: auto;
+  @media (max-width: 1032px) {
+    width: 690px;
+  }
+  @media (max-width: 690px) {
+    width: 340px;
+  }
 `
 
 export const TitleWrapper = styled.div`
@@ -49,6 +92,9 @@ export const BorderDiv = styled.div`
   width: 180px;
   height: 22px;
   display: inline-block;
+  @media (max-width: 690px) {
+    display: none;
+  }
 `
 
 export const BodyWrapper = styled.div`
